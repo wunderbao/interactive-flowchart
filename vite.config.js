5413b26640503429cae9efb7eea9bc4f9387ff60
Jona <8698248+jonamil@users.noreply.github.com>
2023-10-12 21:50:50 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './',
-  build: {
-    rollupOptions: {
-      external: [
-        'narration.m4a'
-      ]
-    }
-  }
+  base: './'
 })
